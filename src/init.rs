c3c3ae234f1f4d03150dd0447e0ba3a2d42b181d
paologaleotti <paologaleotti2002@gmail.com>
2024-02-22 17:48:33 +0100

    feat(core): ✨ added request timeout and panic recovery

--- src/init.rs
+++ src/init.rs
@@ -1,7 +1,11 @@
 use crate::env;
 use crate::routes::make_router;
+
 use axum::Router;
+use std::time::Duration;
 use tower::ServiceBuilder;
+use tower_http::catch_panic::CatchPanicLayer;
+use tower_http::timeout::TimeoutLayer;
 use tower_http::trace::TraceLayer;
 use tracing::Level;
 
@@ -12,5 +16,8 @@ pub fn init_app() -> Router {
 
     let _env = env::init_env();
 
-    make_router().layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()))
+    make_router()
+        .layer(CatchPanicLayer::new())
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()))
+        .layer(TimeoutLayer::new(Duration::from_secs(30)))
 }
