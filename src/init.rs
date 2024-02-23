0eb4521d3331425a58ec912095cd56dc36a88827
paologaleotti <paologaleotti2002@gmail.com>
2024-02-23 10:46:39 +0100

    feat(http): ✨ added cors and sensitive headers layers

--- src/init.rs
+++ src/init.rs
@@ -2,11 +2,12 @@ use crate::env;
 use crate::routes::make_router;
 use crate::shared::core::layers::handle_panic;
 
-use axum::Router;
+use axum::{http::header, Router};
 use std::time::Duration;
-use tower_http::catch_panic::CatchPanicLayer;
-use tower_http::timeout::TimeoutLayer;
-use tower_http::trace::TraceLayer;
+use tower_http::{
+    catch_panic::CatchPanicLayer, cors::CorsLayer, sensitive_headers::SetSensitiveHeadersLayer,
+    timeout::TimeoutLayer, trace::TraceLayer,
+};
 use tracing::Level;
 
 pub fn init_app() -> Router {
@@ -19,5 +20,9 @@ pub fn init_app() -> Router {
     make_router()
         .layer(CatchPanicLayer::custom(handle_panic))
         .layer(TimeoutLayer::new(Duration::from_secs(30)))
+        .layer(SetSensitiveHeadersLayer::new(std::iter::once(
+            header::AUTHORIZATION,
+        )))
+        .layer(CorsLayer::permissive())
         .layer(TraceLayer::new_for_http())
 }
